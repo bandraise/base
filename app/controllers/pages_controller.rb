70847ba01a1f8423aad0d073de61a84f19b89f66
Sam Mienders <sam.mienders@gmail.com>
2017-09-16 20:52:10 +0000

    Created Pages controller.  Added explore, home, index, and rpfile pages.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+  
+  # bank-end code for pages/index
+  def index
+  end
+
+  # bank-end code for pages/index
+  def home
+  end
+
+  # bank-end code for pages/index
+  def profile
+  end
+
+  # bank-end code for pages/index
+  def explore
+  end
+end
